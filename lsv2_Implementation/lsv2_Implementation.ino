4ffebe560396063a77491ba938bb99fc8356dfa6
Pedro Soares <58630295+pcosta18@users.noreply.github.com>
2022-05-21 15:19:49 +0100

    Update lsv2_Implementation.ino
    Já lê uma mensagem com sucesso

--- lsv2_Implementation/lsv2_Implementation.ino
+++ lsv2_Implementation/lsv2_Implementation.ino
@@ -1,13 +1,36 @@
+#include <string>
+
+String ESPrx = "";
+
 void setup() {
-  // put your setup code here, to run once:
-  // SERIE
-  pinMode(2, OUTPUT); //Pino 2 passa a ser saída
-  pinMode(4, INPUT); //Pino 4 passa a ser entrada
+  Serial.begin(9600, SERIAL_8N1);               // Tx0     Rx0 -
+  // 115 200 bts, 8 data bits, 1 stop bit, paraty none
+  Serial2.begin(115200, SERIAL_8N1);            // Por defeito    Rx2-16   Tx2-17
 }
 
-int analogValue;
 void loop() {
 
-  varA = digitalRead(4); // Lê o Pino 4 (0 ou 3,3 V)
-  digitalWrite(2, varA); // ativa/desativa Pino 2 (3,3 ou 0 V)
+  /* Recepção pela porta série 2 (A que vem da CNC) */
+  int bytestoread = Serial.available();
+  if (bytestoread > 0) {
+    Serial.println("tenho estes bytes para ler: ");
+    Serial.println(bytestoread);
+    byte rx[bytestoread];
+    Serial.readBytes(rx, bytestoread);
+
+    for (int i = 0; i < (bytestoread) ; i++) {
+      ESPrx += (char)rx[i];
+    }
+   // Para ver o que foi recebido nesta fase de testes, mais tarde terá que ser removido
+   Serial.println(ESPrx);
+   ESPrx = "";
+
+   //  Testar envio de caracteres especiais
+   byte buff[2] = {16,48};
+   Serial.println("Vou mandar agora os dois bytes especiais");
+   Serial.write(buff,2);
+
+  }
+
+  delay(1);
 }
